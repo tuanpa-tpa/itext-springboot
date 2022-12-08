de46b482f13a24a593ce6fe485e34ca224fd476a
pat <tuanpa.tpa@gmail.com>
2022-12-08 09:02:22 +0700

    json test

--- src/main/java/com/example/itextspringboot/Controller.java
+++ src/main/java/com/example/itextspringboot/Controller.java
@@ -22,6 +22,26 @@ import java.io.IOException;
 public class Controller {
     private final TemplateService templateService;
 
+//    {
+//        "src": "src/main/file/CIST-BM-Bien-ban-nghiem-thu.pdf",
+//            "listText": [{
+//        "text": "Phạm Anh Tuấn",
+//                "positionX": 180,
+//                "positionY": 480,
+//                "width": 250,
+//                "height": 100,
+//                "page": 1
+//    },
+//        {
+//            "text": "CTV",
+//                "positionX": 180,
+//                "positionY": 460,
+//                "width": 250,
+//                "height": 100,
+//                "page": 1
+//        }]
+//    }
+
     @PostMapping("/add")
     public ResponseEntity<?> addText(@RequestBody TextReq textReq) throws DocumentException, IOException {
         templateService.addT(textReq);
